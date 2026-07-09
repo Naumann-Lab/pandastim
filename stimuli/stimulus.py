b023c0eea7dd1f06f27514388304975d72cd2169
Camille  Donoho <camilledonoho@Camilles-MacBook-Pro-316.local>
2026-07-09 18:46:06 -0400

    Edit: change move monocular/ move binocular to be recursive.  UNTESTED!

--- stimuli/stimulus.py
+++ stimuli/stimulus.py
@@ -20,7 +20,7 @@ import math
 from datetime import datetime as dt
 import pandas as pd
 import logging
-
+import time
 from direct.gui.OnscreenText import OnscreenText  # for binocular stim
 from direct.showbase import ShowBaseGlobal
 from direct.showbase.ShowBase import ShowBase
@@ -31,6 +31,7 @@ from panda3d.core import (CardMaker, ClockObject, ColorBlendAttrib, Transparency
 
 from pandastim import utils
 from pandastim.stimuli import stimulus_details, textures
+from panda3d.core.ClockObject import globalClock
 
 
 class StimulusSequencing(ShowBase):
@@ -557,6 +558,8 @@ class StimulusSequencing(ShowBase):
         ]  # rig / implementation specific offset
         self.angle_rotation = 0  # for changing angles on the fly
         self.new_position = 0  # for tracking position on the fly
+        if "tail_lock_buffer" in self.default_params.keys():
+            self.tail_lock_buffer = self.default_params["tail_lock_buffer"]
 
     def format_window(self):
         ShowBaseGlobal.globalClock.setMode(ClockObject.MLimited)
@@ -1744,14 +1747,181 @@ class TailLockedStimulus(BrukerStimulus):
         self.curr_id = 0
         self.next_stimulus = None
         self.strip_angle = 0 #ERR!!!! WARNING!!!! THIS IS PROBABLY WRONG
+        self.curr_right_angle = self.curr_left_angle = 0
+
+    def set_monocular(self):
+        #PLAYGROUND
+        tex = self.current_stimulus.texture.texture
+
+        ## CREATE TEXTURE STAGES ##
+        self.texture_stage = TextureStage("texture_stage")
+
+        ## CREATE CARDS ###
+        cardmaker = CardMaker("stimcard")
+        cardmaker.setFrameFullscreenQuad()
+
+        self.setBackgroundColor((0, 0, 0, 0))
+        self.card = self.aspect2d.attachNewNode(cardmaker.generate())
+        self.card.setAttrib(ColorBlendAttrib.make(ColorBlendAttrib.M_add))
+
+        # ADD TEXTURE STAGES TO CARDS
+        self.card.setTexture(self.texture_stage, tex)
+
+        ### Do the transform things ###
+        self.stage_transform = self.trs_transform_mono(self.current_stimulus.angle)
+        self.card.setTexTransform(self.texture_stage, self.stage_transform)
+
+        self.curr_pos = 0
+        self.taskMgr.add(self.move_monocular, "move_monocular")
+
+    def move_monocular(self, move_monocular_task):
+        if move_monocular_task.time <= self.current_stimulus.stationary_time:
+            # self.new_position = 0
+            pass
+        elif move_monocular_task.time >= self.current_stimulus.duration != -1:
+            self.clear_cards()
+            self.new_position = 0
+            return move_monocular_task.done
+        elif (
+            not np.isnan(self.current_stimulus.hold_after)
+            and move_monocular_task.time >= self.current_stimulus.hold_after
+        ):
+            pass
+        else:#finally moving monucular
+            #MATT CODE THAT WORKS
+            self.new_position = (
+                move_monocular_task.time - self.current_stimulus.stationary_time
+            ) * self.current_stimulus.velocity
+            self.stage_transform = self.trs_transform_mono(self.current_stimulus.angle, self.new_position)
+            self.card.setTexTransform(
+                self.texture_stage, self.stage_transform
+            )
+            # self.card.setTexPos(
+            #     self.texture_stage, self.new_position + self.center_x, self.center_y, 0
+            # )  # u, v, w
+        return move_monocular_task.cont
+
+    def set_binocular(self):
+        tex_1_size = self.current_stimulus.texture[0].texture_size
+        tex_2_size = self.current_stimulus.texture[1].texture_size
+        tex_1 = self.current_stimulus.texture[0].texture
+        tex_2 = self.current_stimulus.texture[1].texture
+
+        ## CREATE TEXTURE STAGES ##
+        self.left_texture_stage = TextureStage("left_texture_stage")
+        self.left_mask = Texture("left_mask_texture")
+        self.left_mask.setup2dTexture(
+            tex_1_size[0], tex_1_size[1], Texture.T_unsigned_byte, Texture.F_luminance
+        )
+        self.left_mask_stage = TextureStage("left_mask_array")
+
+        self.right_texture_stage = TextureStage("right_texture_stage")
+        self.right_mask = Texture("right_mask_texture")
+        self.right_mask.setup2dTexture(
+            tex_2_size[0], tex_2_size[1], Texture.T_unsigned_byte, Texture.F_luminance
+        )
+        self.right_mask_stage = TextureStage("right_mask_stage")
+
+        ## CREATE CARDS ###
+        cardmaker = CardMaker("stimcard")
+        cardmaker.setFrameFullscreenQuad()
+
+        self.setBackgroundColor((0, 0, 0, 1))
+        self.left_card = self.aspect2d.attachNewNode(cardmaker.generate())
+        self.left_card.setAttrib(ColorBlendAttrib.make(ColorBlendAttrib.M_add))
+        self.right_card = self.aspect2d.attachNewNode(cardmaker.generate())
+        self.right_card.setAttrib(ColorBlendAttrib.make(ColorBlendAttrib.M_add))
+
+        # CREATE MASK ARRAYS
+        self.left_mask_array = 255 * np.ones(
+            (tex_1_size[0], tex_1_size[1]), dtype=np.uint8
+        )
+        self.left_mask_array[
+            :, (tex_1_size[1] // 2) - self.current_stimulus.strip_width // 2 :
+        ] = 0
+
+        self.right_mask_array = 255 * np.ones(
+            (tex_2_size[0], tex_2_size[1]), dtype=np.uint8
+        )
+        self.right_mask_array[
+            :, : (tex_2_size[1] // 2) + self.current_stimulus.strip_width // 2
+        ] = 0
+
+        if self.default_params["projecting_fish"]:
+            ### DANGER ZONE ###
+            ### currently assumes 1024 textures ###
+            self.left_mask_array[506:515, 511:512] = 120
+            self.right_mask_array[506:515, 512:513] = 120
+            self.left_mask_array[514:516, 510:512] = 255
+            self.right_mask_array[514:516, 512:514] = 255
+            ### END DANGER ZONE ###
+
+        # ADD TEXTURE STAGES TO CARDS
+        self.left_mask.setRamImage(self.left_mask_array)
+        self.left_card.setTexture(self.left_texture_stage, tex_1)
+
+        # Multiply the texture stages together
+        self.left_mask_stage.setCombineRgb(
+            TextureStage.CMModulate,
+            TextureStage.CSTexture,
+            TextureStage.COSrcColor,
+            TextureStage.CSPrevious,
+            TextureStage.COSrcColor,
+        )
+
+        self.left_card.setTexture(self.left_mask_stage, self.left_mask)
+
+        # ADD TEXTURE STAGES TO CARDS
+        self.right_mask.setRamImage(self.right_mask_array)
+        self.right_card.setTexture(self.right_texture_stage, tex_2)
+
+        # Multiply the texture stages together
+        self.right_mask_stage.setCombineRgb(
+            TextureStage.CMModulate,
+            TextureStage.CSTexture,
+            TextureStage.COSrcColor,
+            TextureStage.CSPrevious,
+            TextureStage.COSrcColor,
+        )
+
+        self.right_card.setTexture(self.right_mask_stage, self.right_mask)
+
+        ### Do the transform things ###
+        self.mask_transform = self.trs_transform()
+
+        self.left_angle = (
+            self.current_stimulus.strip_angle
+            + self.current_stimulus.angle[0]#because that binocular stim is set up relative to the strip
+            + self.rotation_offset
+        )
+        self.right_angle = (
+            self.current_stimulus.strip_angle
+            + self.current_stimulus.angle[1]#because that binocular stim is set up relative to the strip
+            + self.rotation_offset
+        )
+
+        self.left_card.setTexTransform(self.left_mask_stage, self.mask_transform)
+        self.right_card.setTexTransform(self.right_mask_stage, self.mask_transform)
+
+        # Left texture
+        self.left_card.setTexScale(self.left_texture_stage, 1 / self.scale)
+        self.left_card.setTexRotate(self.left_texture_stage, self.left_angle)
+
+        # Right texture
+        self.right_card.setTexScale(self.right_texture_stage, 1 / self.scale)
+        self.right_card.setTexRotate(self.right_texture_stage, self.right_angle)
+
+        # start the movement once everything is set up
+        self.curr_pos = self.current_stimulus.position[0]
+        self.taskMgr.add(self.move_binocular, "move_binocular")
 
     def move_binocular(self, move_binocular_task):
         ### LEFT SIDE ###
         if move_binocular_task.time <= self.current_stimulus.stationary_time[0]:
-            new_position_left = 0
+            step = 0
         elif move_binocular_task.time >= self.current_stimulus.duration[0] != -1:
             if self.default_params["hold_onfinish"]:
-                new_position_left = self.new_position[0]
+                step_left = 0
             else:
                 self.left_card.detach_node()
                 new_position_left = None
@@ -1759,26 +1929,23 @@ class TailLockedStimulus(BrukerStimulus):
             not np.isnan(self.current_stimulus.hold_after[0])
             and move_binocular_task.time >= self.current_stimulus.hold_after[0]
         ):
-            new_position_left = self.new_position[0]
+            step_left = 0
         else:
-            new_position_left = (move_binocular_task.time * self.current_stimulus.velocity[0])#remove -time because it works now
+            step_left = math.cos(self.curr_left_angle) * (globalClock.getDt() * self.current_stimulus.velocity[0])
+            new_position_left = self.curr_pos + step_left
             self.left_card.setTexPos(
                 self.left_texture_stage,
-                new_position_left + self.current_stimulus.position[0],
+                new_position_left,
                 self.current_stimulus.position[1],
                 0,
             )  # u, v, w
 
         ### RIGHT SIDE ###
-        # self.tlmot = self.current_stimulus["taillock_on_motion"]
-        # self.tlstat = self.current_stimulus["taillock_stationary"]
-        print("COM REF:", self.current_stimulus)
-
         if move_binocular_task.time <= self.current_stimulus.stationary_time[1]:
-            new_position_right = 0
+            step_right = 0
         elif move_binocular_task.time >= self.current_stimulus.duration[1] != -1:
             if self.default_params["hold_onfinish"]:
-                new_position_right = self.new_position[1]
+                step_right = 0
             else:
                 self.right_card.detach_node()
                 new_position_right = None
@@ -1786,9 +1953,31 @@ class TailLockedStimulus(BrukerStimulus):
             not np.isnan(self.current_stimulus.hold_after[1])
             and move_binocular_task.time >= self.current_stimulus.hold_after[1]
         ):
-            new_position_right = self.new_position[1]
+            step_right = 0
         else:
-            new_position_right = (move_binocular_task.time * (self.current_stimulus.velocity[1] + self.forward_swimming))#added forward swim adjustment
+            step_right = math.cos(self.curr_right_angle) * (globalClock.getDt() * self.current_stimulus.velocity[1])
+            new_position_right = self.curr_pos + step_right
+            self.right_card.setTexPos(
+                self.right_texture_stage,
+                new_position_right,
+                self.current_stimulus.position[1],
+                0,
+            )  # u, v, w
+
+        self.new_position = new_position_left, new_position_right
+
+        if move_binocular_task.time >= max(
+            self.current_stimulus.duration[0], self.current_stimulus.duration[1]
+        ):
+            self.clear_cards()
+            return move_binocular_task.done
+
+        return move_binocular_task.cont
+
+
+    def move_cards_live(self):
+            if time.time() - self.moving_task_start < self.current_stimulus.stationary_time[1]:
+                
             self.right_card.setTexPos(
                 self.right_texture_stage,
                 new_position_right + self.current_stimulus.position[0],
@@ -1798,7 +1987,26 @@ class TailLockedStimulus(BrukerStimulus):
 
 
 
+
+
+        # match self.current_stimulus:
+        #     case stimulus_details.MonocularStimulusDetails():
+        #         pass
+        #     case stimulus_details.BinocularStimulusDetails():
+        #         pass
+        #     case stimulus_details.MaskedStimulusDetailsPack():
+        #         print("Sorry, MaskedStimulusDetails have not been implemented")
+        #     case _:
+        #         print(
+        #             f"{self.current_stimulus.__class__} -- Stimulus type not understood, transform failed"
+        #         )
+
+
+
     def set_transforms(self):
+        self.set_transform_calltime = time.time()
+        self.last_update + self.default_params
+
         match self.current_stimulus:
             case stimulus_details.MonocularStimulusDetails():
 
@@ -1831,14 +2039,16 @@ class TailLockedStimulus(BrukerStimulus):
                     self.left_mask_stage, self.mask_transform
                 )
                 self.left_card.setTexScale(self.left_texture_stage, 1 / self.scale)
-                self.left_card.setTexRotate(self.left_texture_stage, self.left_angle)
+                self.curr_left_angle  = self.left_angle + self.turning
+                self.left_card.setTexRotate(self.left_texture_stage, self.curr_left_angle)
 
                 # Right texture
                 self.right_card.setTexTransform(
                     self.right_mask_stage, self.mask_transform
                 )
                 self.right_card.setTexScale(self.right_texture_stage, 1 / self.scale)
-                self.right_card.setTexRotate(self.right_texture_stage, self.right_angle)
+                self.curr_right_angle = self.right_angle + self.turning
+                self.right_card.setTexRotate(self.right_texture_stage, self.curr_right_angle)
 
             case _:
                 print(
@@ -1850,6 +2060,7 @@ class TailLockedStimulus(BrukerStimulus):
             self.turning = self.updating_info[0]
             self.forward_swimming = self.updating_info[1]
             self.set_transforms()
+            self.last_update = time.time()
         else:
             print(f"ERR: Recieved data is of length {len(self.updating_info)}.  It expected to be of length 2.")
 
@@ -1869,32 +2080,7 @@ class TailLockedStimulus(BrukerStimulus):
         #         self.center_x, self.center_y, self.angle_rotation = self.updating_info
         #         self.strip_angle = self.angle_rotation + self.rotation_offset
         #         self.set_transforms()
-    def buddy_task(self, buddytask):
-        """talk to a stytrabuddy about what task the buddy should do"""
 
-        self.buddy.pauseStatus(self.paused)
-        self.buddy.position(self.new_position)
-        self.buddy.stimulus(self.current_stimulus)
-        print("CSTEST",self.buddy.stimulus)
-        self.updating, self.updating_info = self.buddy.request_updating()
-        self.next_stimulus = self.buddy.request_stimulus()#request next stimulus from buddy
-        if self.next_stimulus is not None:#if there is a stimulus change
-            if isinstance(self.next_stimulus, stimulus_details.MonocularStimulusDetails)\
-                or isinstance(self.next_stimulus, stimulus_details.BinocularStimulusDetails)\
-                or isinstance(self.next_stimulus, stimulus_details.MaskedStimulusDetailsPack):#handles input of stimulus object
-                self.clear_cards()
-                self.current_stimulus = self.next_stimulus
-                self.next_stimulus = None
-                self.set_stimulus()
-            else:
-                print('stim input to stimulus.py has to be an object belong to stimulus_details class')
-        elif self.updating:
-            self.update_stimulus()
-        self.buddy.broadcaster()#slight delay of one round in reporting new stimulus because we have to wait till buddy is updated faster
-
-        return buddytask.cont
-
-        
     
 
 
